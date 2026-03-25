c2ab8d4890630325d8d32d551f8d89f0b346f2a7
Vidhya Selvaraj <smile2login@gmail.com>
2026-03-25 18:49:08 +0530

    chore: untrack ignored playwright files

---
+++ tests/Week2_Day1/launchBrowser.spec.ts
@@ -0,0 +1,24 @@
+/* Browser
+Browsercontext ->window (invidual isolated profile + incognito window//Store any data)
+page 
+
+ 
+/* const browser = chromium.launch()
+const context = browser.newContext()
+const page = context.newPage() */
+
+
+import { test,chromium} from '@playwright/test'
+
+test('Launch the browser', async()=>{
+ const browser=await chromium.launch({channel:"msedge",headless: false})
+ const context=await browser.newContext()
+ const page= await context.newPage()
+ await page.goto("http://leaftaps.com/opentaps/control/main")
+ await page.waitForTimeout(5000)
+ //await page.pause();
+ 
+})
+
+
+
